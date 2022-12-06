50581790f090ccd163af3432d1150ac2a7cffbe3
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-06 11:41:53 +0000

    [Add]public/itemsコントローラ作成、ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
   root to: "public/homes#top"
 
   namespace :admin do
@@ -8,6 +12,7 @@ Rails.application.routes.draw do
   end
 
   namespace :public do
+    resources :items, only: [:index, :show]
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
 
