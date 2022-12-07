5b1328f033a89d7163c6e81ff01dbc382a58ea19
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-07 12:25:27 +0000

    [Add]管理者ログイン後の遷移先変更予定

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-   def after_sign_in_path_for(resource)
+
+  def after_sign_in_path_for(resource)
     admin_homes_top
-   end
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configre_permitted_parameters
