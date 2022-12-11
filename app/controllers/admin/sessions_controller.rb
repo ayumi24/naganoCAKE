b7fea37c4a51f871b70f87cf678828fb110d647b
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-11 08:43:04 +0000

    [Add]headerの編集

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,10 +2,6 @@
 
 class Admin::SessionsController < Devise::SessionsController
 
-  def after_sign_in_path_for(resource)
-    admin_homes_top_path
-  end
-
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+    
+  def after_sign_in_path_for(resource)
+    
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
