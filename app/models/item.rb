7b0e08f93a63f1abbeb64fd49f933e1bf8f03e78
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-11 06:17:14 +0000

    [Fix]商品詳細ページでno-imageが表示できるように修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,6 @@ class Item < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [100,100]).processed
+    image
   end
 end
