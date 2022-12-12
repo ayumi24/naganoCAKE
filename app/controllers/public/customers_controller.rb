3579d61283dd9333600c0bf40d6a7d0a0e765baa
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-12 10:53:37 +0000

    [Add]会員情報編集機能(ストロングパラメータ追記)

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,4 +12,11 @@ class Public::CustomersController < ApplicationController
     customer.update(customer_params)
     redirect_to public_customer_path
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+  end
+
 end
