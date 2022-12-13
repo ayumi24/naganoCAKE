5fca582aeea6fca7b8d447f1c9441b885bc90b6e
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-13 12:53:24 +0000

    [Fix]新規商品登録ルーティング修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,13 +4,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
-    @new_item = Item.new
+    @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_item_path
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
