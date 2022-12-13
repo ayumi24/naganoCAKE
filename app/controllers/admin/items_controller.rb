4ec12df359e6fae6eb1b68295101f6dc5b2fa18e
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-13 12:02:17 +0000

    新規商品登録ボタン編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,13 +4,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new
+    @new_item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_items_path
+    redirect_to admin_item_path
   end
 
   def show
