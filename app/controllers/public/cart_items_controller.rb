2ee2aaabc280edbc3efbbfa6aab6ff38184e719c
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-14 12:05:16 +0000

    [Add]カート追加機能作成（要修正）

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -13,5 +13,16 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:id])
+    @cart_item = Cart_item.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to public_cart_items(@cart_item.id)
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:amount)
   end
 end
