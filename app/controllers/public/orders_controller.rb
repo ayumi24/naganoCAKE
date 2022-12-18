4a9a95e438324b13c823816ba729662ebbf18626
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-18 08:59:32 +0000

    [Add]配送先登録途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,4 +10,18 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  def confirm
+    @order = Order.new(order_params)
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+  end
+
+  private
+
+  def order_params
+  params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
 end
