2c53793be51b78a4abca877cd8bc77cf18d2436b
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-19 10:24:07 +0000

    [Add]adminログアウト後の遷移先変更

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -18,13 +18,23 @@ class Public::OrdersController < ApplicationController
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:address_number] == "2"
+      address_new = current_customer.addresses.new(address_params)
+      if address_new.save
+      else
+        render :new
+      end
     end
   end
 
   private
 
   def order_params
-  params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  params.require(:order).permit(:payment_method, :postal_code, :address, :name, :price)
+  end
+
+  def address_params
+    params.require(:order).permit(:postal_code, :address, :name)
   end
 
 end
