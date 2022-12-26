53b2b4ce66b175b624bf1fa665b61018c48445da
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-12-26 11:26:49 +0000

    [Add]会員側会員情報編集画面レイアウト調整

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Public::RegistrationsController < Devise::RegistrationsController
 
   def after_sign_in_path_for(resource)
-    public_customer_path(resource)
+    public_customers_path(resource)
   end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
